604960018eed99caa013d96876d43e215b09f244
Aadan4762 <aadan4762@gmail.com>
2024-06-28 02:51:06 -0800

    Initial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
