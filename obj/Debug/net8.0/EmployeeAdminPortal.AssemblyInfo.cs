49f99e260c811a625a1089b0d122ea7d6fb28aad
Aadan4762 <aadan4762@gmail.com>
2024-08-01 14:12:09 +0300

    branch

--- obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b290015dbea8610cf84736960bb98c978182ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2206daa207f0c1317a00ce1974ccf80e193373dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
